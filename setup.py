568229640486156d1b337b144664939c7b8f3b0a
Kozo Nishida <knishida@riken.jp>
2014-09-01 21:47:46 +0900

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cytoscape',
+      version='1.0',
+      description='Cytoscape visualization with IPython Notebook',
+      author='Keiichiro Ono',
+      author_email='kono@ucsd.edu',
+      url='http://cytoscape.org',
+      packages=['cytoscape'],
+     )
