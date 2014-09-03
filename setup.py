cb805756c17733d7ba66fc7addf68cdf4fd0dc14
Kozo Nishida <knishida@riken.jp>
2014-09-04 00:51:34 +0900

    rename package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='icytoscapy',
+      version='1.0',
+      description='Cytoscape visualization with IPython Notebook',
+      author='Naoki Nishida, Keiichiro Ono, Kozo Nishida',
+      author_email='knishida@riken.jp',
+      url='https://github.com/kozo2/cytoscape-ipy',
+      packages=['icytoscapy'],
+     )
